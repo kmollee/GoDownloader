4b991df7ac281bccab13b366153b1b2ea8edbe97
kmollee <admin@mongkonglee.com>
2019-04-18 02:15:30 +0800

    able start worker and download file

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # GoDownloader
+
+
+
+1. fetch size and rangeable
+2. if rangeable
+        2.1 if not: download file directly. **out**
+3. split into serval chunk
+        3.1. Chunk{path: size:}
+4. launch workers
+5. send chunk to workers
+6. worker get chunk, start download
+        6.1 check file exist or not and checksum
+        6.2 if file already download return finish chunck
+        6.3 if not, start download
+
+7. finish chunck / split chunck num == 1; merge all chunk into one
--- main.go
+++ main.go
@@ -8,34 +8,118 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"sync"
 
 	"github.com/pkg/errors"
+)
+
+type ByteSize uint64
+
+const (
+	B  ByteSize = 1
+	KB ByteSize = 1 << (10 * iota)
+	MB
+	GB
+	TB
+	PB
+	EB
+)
+
+const (
+	MinChunkSize int64 = int64(1 * MB)
+	tmpPath            = "./.godownloadtmp"
 )
 
 func main() {
 	url := flag.String("u", "", "the url to download")
 	output := flag.String("o", "", "output path")
+	worker := flag.Int("w", 6, "worker to download")
 	flag.Parse()
 
 	fmt.Printf("url: %s output: %s", *url, *output)
 
-	c := http.Client{}
-	_, isRangeAccept, err := fetchResourceLength(c, *url)
-	log.Printf("isRangeAccept: %v", isRangeAccept)
+	client := http.DefaultClient
+
+	size, isRangeAccept, err := fetchResourceLength(client, *url)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "ERR: %v", err)
 		os.Exit(1)
 	}
 
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	_ = cancel
+
 	if isRangeAccept {
+
+		chunks := newChunks(size)
+
+		m := MultiDownloader{client: client, worker: *worker, ctx: ctx, chunks: chunks, url: *url, chunk: make(chan *Chunk), done: make(chan struct{}), err: make(chan error)}
+		err := m.Start()
+		if err != nil {
+			log.Fatal(err)
+		}
+		// go m.Display()
 
 	} else {
 		log.Println("not accept multiple downloader, start single downloader to downloader file")
-		if err := downloadFile(c, *url, *output, ""); err != nil {
+		if err := downloadFile(*client, *url, *output, ""); err != nil {
 			log.Fatal(err)
 		}
 	}
+
+}
+
+type Chunk struct {
+	start int64
+	end   int64
+	path  string
+	size  int64
+}
+
+func (c *Chunk) create() error {
+	f, err := os.OpenFile(c.path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0660)
+	if nil != err {
+		return errors.Wrapf(err, "could not create dst file: %s", c.path)
+	}
+	f.Close()
+
+	return nil
+}
+
+func (c *Chunk) isDone() (bool, error) {
+	f, err := os.Stat(c.path)
+	if err != nil {
+		return false, errors.Wrapf(err, "could not stat file: %s", c.path)
+	}
+
+	log.Printf("compare size: %v want %v", f.Size(), c.size)
+	return f.Size() == c.size, nil
+}
+
+func newChunks(size int64) []*Chunk {
+	log.Printf("start create chunk")
+	chunks := make([]*Chunk, 0)
+
+	chuckID := 0
+	var start int64
+loop:
+	for size > 0 {
+		chunkPath := fmt.Sprintf("chunk-%d", chuckID)
+
+		if size < MinChunkSize {
+			c := &Chunk{path: chunkPath, size: size, start: start, end: start + size}
+			chunks = append(chunks, c)
+			break loop
+		}
+		c := &Chunk{path: chunkPath, size: MinChunkSize, start: start, end: start + MinChunkSize}
+		chunks = append(chunks, c)
+
+		chuckID++
+		start += MinChunkSize
+		size -= MinChunkSize
+	}
+
+	return chunks
 
 }
 
@@ -46,7 +130,8 @@ func main() {
 // 	return nil
 // }
 
-func fetchResourceLength(client http.Client, url string) (int64, bool, error) {
+func fetchResourceLength(client *http.Client, url string) (int64, bool, error) {
+	log.Printf("URL: %v", url)
 	res, err := client.Head(url)
 	if err != nil {
 		return 0, false, errors.Wrap(err, "could not get url")
@@ -68,31 +153,106 @@ func fetchResourceLength(client http.Client, url string) (int64, bool, error) {
 
 }
 
-const MinChunkSize = 100000
-
 type MultiDownloader struct {
-	c       http.Client
-	worker  int
-	src     string
-	dst     string
-	job     chan int64
-	ctx     context.Context
-	tmpfile []string
-	chunk   int64
-	lock    sync.Mutex
+	client *http.Client
+	worker int
+
+	url string
+
+	chunks []*Chunk
+	chunk  chan *Chunk
+	err    chan error
+
+	done      chan struct{}
+	doneCount int
+
+	ctx context.Context
 }
 
-// func (m *MultiDownloader) Start() error {
-// 	for i := 0; i < m.worker; i++ {
-// 		go func(ctx context.Context, job <-chan int64) {
-// 			for j := range job {
-// 			}
-// 		}(m.ctx, m.job)
-// 	}
-// 	return nil
-// }
+func (m *MultiDownloader) startWorker() {
+
+	for c := range m.chunk {
+		log.Printf("start receive chunk")
+
+		log.Printf("create chunk")
+		if err := c.create(); err != nil {
+			m.err <- err
+			return
+		}
+
+		log.Printf("check chunk")
+		done, err := c.isDone()
+		if err != nil {
+			m.err <- err
+			return
+		}
+
+		if done {
+			m.done <- struct{}{}
+			continue
+		}
+
+		byterange := fmt.Sprintf("%d-%d", c.start, c.end)
+		log.Printf(">>>> download chunk %s", byterange)
+		if err := downloadFile(*m.client, m.url, c.path, byterange); err != nil {
+			m.err <- err
+			return
+		}
+		m.done <- struct{}{}
+	}
+
+}
+
+func (m *MultiDownloader) startFeeder() {
+	for i := range m.chunks {
+		m.chunk <- m.chunks[i]
+	}
+}
+
+func (m *MultiDownloader) mergeChunks() {
+	log.Printf("merge chunks")
+	// for _, c := range m.chunks {
+	// 	m.chunk <- c
+	// }
+}
+
+func (m *MultiDownloader) Start() error {
+	log.Printf("chunks: %v", m.chunks)
+	log.Printf("start worker")
+
+	if m.worker > len(m.chunks) {
+		m.worker = len(m.chunks)
+	}
+
+	for i := 0; i < m.worker; i++ {
+		go m.startWorker()
+	}
+	log.Printf("start feeder")
+	go m.startFeeder()
+
+	log.Printf("start wait all finish")
+
+loop:
+	for {
+		select {
+		case <-m.done:
+			m.doneCount++
+			if m.doneCount == len(m.chunks) {
+				break loop
+			}
+		case err := <-m.err:
+			close(m.chunk)
+			return err
+		}
+	}
+
+	m.mergeChunks()
+
+	return nil
+}
 
 func downloadFile(client http.Client, src, dst, rang string) error {
+	log.Printf("start download %s to %s [%s]", src, dst, rang)
 
 	req, err := http.NewRequest(http.MethodGet, src, nil)
 	if err != nil {
